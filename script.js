737807789bf9e887fbf4ccaed6d7e0a03bfd3d25
Ally Glenday <allyglenday@allys-air.home>
2022-08-13 11:30:42 +0100

    Add all remaining pages

---
+++ script.js
@@ -0,0 +1,73 @@
+// var yourAudio = document.getElementById('yourAudio'),
+//     ctrl = document.getElementById('audioControl');
+
+// ctrl.onclick = function () {
+
+//     // Update the Button
+//     var pause = ctrl.innerHTML === 'pause!';
+//     ctrl.innerHTML = pause ? 'play!' : 'pause!';
+
+//     // Update the Audio
+//     var method = pause ? 'pause' : 'play';
+//     yourAudio[method]();
+
+//     // Prevent Default Action
+//     return false;
+// };
+
+
+// function myFunction() {
+//     // Declare variables
+//     var input, filter, ul, li, a, i, txtValue;
+//     input = document.getElementById('myInput');
+//     filter = input.value.toUpperCase();
+//     ul = document.getElementById("myUL");
+//     li = ul.getElementsByTagName('li');
+  
+//     // Loop through all list items, and hide those who don't match the search query
+//     for (i = 0; i < li.length; i++) {
+//       a = li[i].getElementsByTagName("a")[0];
+//       txtValue = a.textContent || a.innerText;
+//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//         li[i].style.display = "";
+//       } else {
+//         li[i].style.display = "none";
+//       }
+//     }
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
+
+
+
+  
